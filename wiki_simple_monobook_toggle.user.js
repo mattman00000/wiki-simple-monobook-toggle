86c79aa9926efbe88392e828f16de2ca860fcd02
mattman00000 <mattman00000@gmail.com>
2016-03-22 04:53:16 -0700

    0.0.1

--- wiki_simple_monobook_toggle.user.js
+++ wiki_simple_monobook_toggle.user.js
@@ -1,5 +1,21 @@
-//javascript: //from bookmarklet version
-if (!window.location.href.includes("monobook"))
-{
-window.location=window.location.href.substring(0,(window.location.href.includes("#")?window.location.href.lastIndexOf("#"):window.location.href.length)).concat((window.location.search?"&":"?")+"useskin=monobook");
-}
+// ==UserScript==
+// @name         Wikimedia Monobook Toggle
+// @namespace    https://github.com/mattman00000
+// @version      0.0.1
+// @description  try to take over the world!
+// @author       mattman00000
+// @match        *://*.wikia.com/*
+// @match        *://*.wikipedia.org/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+	'use strict';
+	
+	// Your code here...
+	
+	//javascript: //from bookmarklet version
+	if (!window.location.search.includes("useskin=monobook")) {
+		window.location.assign(window.location.protocol+"//"+window.location.host+window.location.pathname+window.location.search+((window.location.search?"&":"?")+"useskin=monobook")+window.location.hash
+	}
+})();
